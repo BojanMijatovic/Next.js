250d23778d4b6ba637380ace4e1a4674c2096840
Bojan Mijatovic <bojan.freelance.jobs@gmail.com>
2021-08-04 22:17:50 +0200

    add

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
-const ComponentA = () => {
+//  ComponentA
+const ComponentA = (props) => {
   const [num, setNum] = useState(5);
 
   const addNum = () => {
@@ -12,44 +13,72 @@ const ComponentA = () => {
   };
 
   return (
-    <React.Fragment>
-      <h2>From Component A</h2>
-      <button onClick={addNum}>add +</button>
-      <button onClick={removeNum}>remove -</button>
-      <h4>Num: {num}</h4>
-      <ComponentC />
-    </React.Fragment>
+    <div className=''>
+      <h2>Component A</h2>
+      <div className=''>
+        <h2>Here is from Home {props.numHome}</h2>
+        <h2>String : {props.strHome}</h2>
+        <h2>Object : {props.objHome.obj.a}</h2>
+        <div className=''>
+          <h2> Function: {<props.funcHome />}</h2>
+        </div>
+      </div>
+    </div>
   );
 };
 
+//  ComponentC
 class ComponentC extends React.Component {
-  state = {
-    myValue: 10,
-  };
+  constructor() {
+    super();
+    this.state = { myValue: 0 };
+  }
 
-  changeState(increment) {
-    // this.changeState({
-    //   myValue: increment,
-    // });
-    console.log(increment);
+  changeState(incrementor) {
+    this.setState({ myValue: incrementor });
   }
 
   render() {
+    const { myValue } = this.state;
+
     return (
       <>
-        <h1>From C</h1>
-        <button onClick={() => this.changeState(this.state.myValue + 2)}>add + 2</button>
-        <h2>{this.state.myValue}</h2>
+        <h2>From C: {this.state.myValue}</h2>
+        <button onClick={() => this.changeState(myValue + 2)}>add + 2</button>
+        <button onClick={() => this.changeState(myValue - 2)}>remove - 2</button>
+        <div className=''>
+          <h1>From Home {this.props.numHome}</h1>
+        </div>
       </>
     );
   }
 }
 
+//  Component Home
 const Home = () => {
+  const [num, setNum] = useState(5);
+  const [num1, setNum1] = useState(5);
+
+  useEffect(() => {
+    console.log('When state is changed');
+  }, [num]);
+
   return (
     <React.Fragment>
-      <h1>Hello World </h1>
-      <ComponentA />
+      {/* <button onClick={() => setNum(num + 1)}>add +</button>
+      <button onClick={() => setNum(num - 1)}>remove -</button>
+      <h2>Num: {num}</h2> */}
+      {/* <ComponentA
+        numHome={num}
+        strHome={'here is string'}
+        objHome={{
+          obj: {
+            a: 'home object from props',
+          },
+        }}
+        funcHome={() => <div>function from Home</div>}
+      /> */}
+      <ComponentC numHome={num} />
     </React.Fragment>
   );
 };
